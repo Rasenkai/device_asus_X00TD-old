LineageOS/android_device_asus_X00TD

X00TD: releasetools: make modem check more user friendly
Change-Id: Ib334abf1d53ba889d1b4f746657336fae2d47db2
Signed-off-by: Albert I <krascgq@outlook.co.id>
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

releasetools.py
@@ -29,10 +29,15 @@ def IncrementalOTA_Assertions(info):
 def AddModemAssertion(info):
   android_info = info.input_zip.read("OTA/android-info.txt")
   m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
-  if m:
-    version = m.group(1).rstrip()
-    if len(version) and '*' not in version:
-      cmd = 'assert(X00T.verify_modem("' + version + '") == "1");'
+  f = re.search(r'require\s+version-firmware\s*=\s*(.+)', android_info)
+  if m and f:
+    version_modem = m.group(1).rstrip()
+    version_firmware = f.group(1).rstrip()
+    if ((len(version_modem) and '*' not in version_modem) and \
+    (len(version_firmware) and '*' not in version_firmware)):
+      cmd = 'assert(X00T.verify_modem("' + version_modem + '") == "1" || \
+abort("Error: This package requires firmware version ' + version_firmware + \
+' or newer. Please upgrade firmware and retry!"););'
       info.script.AppendExtra(cmd)
   return
 

Rasenkai/device_asus_X00TD-old

Signed-off-by: Rasenkai <rasenkai99@gmail.com>

releasetools.py
@@ -29,9 +29,14 @@ def IncrementalOTA_Assertions(info):
 def AddModemAssertion(info):
   android_info = info.input_zip.read("OTA/android-info.txt")
   m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
-  if m:
-    version = m.group(1).rstrip()
-    if len(version) and '*' not in version:
-      cmd = 'assert(X00TD.verify_modem("' + version + '") == "1");'
+  f = re.search(r'require\s+version-firmware\s*=\s*(.+)', android_info)
+  if m and f:
+    version_modem = m.group(1).rstrip()
+    version_firmware = f.group(1).rstrip()
+    if ((len(version_modem) and '*' not in version_modem) and \
+    (len(version_firmware) and '*' not in version_firmware)):
+      cmd = 'assert(X00TD.verify_modem("' + version_modem + '") == "1" || \
+abort("Error: This package requires firmware version ' + version_firmware + \
+' or newer. Please upgrade firmware and retry!"););'
       info.script.AppendExtra(cmd)
   return