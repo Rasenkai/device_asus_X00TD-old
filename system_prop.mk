LineageOS/android_device_asus_X00TD

X00TD: Migrate system props to separate system_prop.mk
* Also add ro.hq.project into system prop

Change-Id: Iff7fe577699a729c2487196415ff052b2e65107c
Signed-off-by: Albert I <krascgq@outlook.co.id>
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

system_prop.mk
@@ -1,0 +1,7 @@
+# GMS
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.com.google.clientidbase.ms=android-asus-tpin
+
+# Huaqin project
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.hq.project=ZQL1650

Rasenkai/device_asus_X00TD-old

Signed-off-by: Rahul Krishna <Rahulkrishna585@gmail.com>

system_prop.mk
@@ -1,0 +1,7 @@
+# GMS
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.com.google.clientidbase.ms=android-asus-tpin
+
+# Huaqin project
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.hq.project=ZQL1820