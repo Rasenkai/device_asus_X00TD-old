LineageOS/android_device_asus_X00TD

X00TD: assert on modem build timestamp
krasCGQ:
* Drop TrustZone assertion in favor of this commit
* Adjust for sdm660 eMMC mount point

Change-Id: Ifbcd24205a11efbecb0b55d4ddaf30178e245c28
Signed-off-by: Alexander Martinz <alex@amartinz.at>
Signed-off-by: Albert I <krascgq@outlook.co.id>
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

recovery/recovery_updater.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016, The CyanogenMod Project
+ * Copyright (C) 2015, The CyanogenMod Project
  * Copyright (C) 2017, The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,22 +24,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
-#include <string>
-#include <vector>
-
 #include "edify/expr.h"
-#include "otautil/error_code.h"
+#include "updater/install.h"
 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
 #define ALPHABET_LEN 256
 
-#define TZ_PART_PATH "/dev/block/bootdevice/by-name/tz"
-#define TZ_VER_STR "QC_IMAGE_VERSION_STRING="
-#define TZ_VER_STR_LEN 24
-#define TZ_VER_BUF_LEN 255
+#ifdef USES_BOOTDEVICE_PATH
+#define MODEM_PART_PATH "/dev/block/bootdevice/by-name/modem"
+#else
+#define MODEM_PART_PATH "/dev/block/platform/soc/c0c4000.sdhci/by-name/modem"
+#endif
+#define MODEM_VER_STR "Time_Stamp\": \""
+#define MODEM_VER_STR_LEN 14
+#define MODEM_VER_BUF_LEN 20
 
 /* Boyer-Moore string search implementation from Wikipedia */
 
@@ -119,75 +121,81 @@ static char * bm_search(const char *str, size_t str_len, const char *pat,
     return NULL;
 }
 
-static int get_tz_version(char *ver_str, size_t len) {
+static int get_modem_version(char *ver_str, size_t len) {
     int ret = 0;
     int fd;
-    int tz_size;
-    char *tz_data = NULL;
+    int modem_size;
+    char *modem_data = NULL;
     char *offset = NULL;
 
-    fd = open(TZ_PART_PATH, O_RDONLY);
+    fd = open(MODEM_PART_PATH, O_RDONLY);
     if (fd < 0) {
         ret = errno;
         goto err_ret;
     }
 
-    tz_size = lseek64(fd, 0, SEEK_END);
-    if (tz_size == -1) {
+    modem_size = lseek64(fd, 0, SEEK_END);
+    if (modem_size == -1) {
         ret = errno;
         goto err_fd_close;
     }
 
-    tz_data = (char *) mmap(NULL, tz_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (tz_data == (char *)-1) {
+    modem_data = (char *) mmap(NULL, modem_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (modem_data == (char *)-1) {
         ret = errno;
         goto err_fd_close;
     }
 
-    /* Do Boyer-Moore search across TZ data */
-    offset = bm_search(tz_data, tz_size, TZ_VER_STR, TZ_VER_STR_LEN);
+    /* Do Boyer-Moore search across MODEM data */
+    offset = bm_search(modem_data, modem_size, MODEM_VER_STR, MODEM_VER_STR_LEN);
     if (offset != NULL) {
-        strncpy(ver_str, offset + TZ_VER_STR_LEN, len);
+        snprintf(ver_str, len, "%s", offset + MODEM_VER_STR_LEN);
     } else {
         ret = -ENOENT;
     }
 
-    munmap(tz_data, tz_size);
+    munmap(modem_data, modem_size);
 err_fd_close:
     close(fd);
 err_ret:
     return ret;
 }
 
-/* verify_trustzone("TZ_VERSION", "TZ_VERSION", ...) */
-Value * VerifyTrustZoneFn(const char *name, State *state,
-                     const std::vector<std::unique_ptr<Expr>>& argv) {
-    char current_tz_version[TZ_VER_BUF_LEN];
+/* verify_modem("MODEM_VERSION") */
+Value * VerifyModemFn(const char *name, State *state, int argc, Expr *argv[]) {
+    char current_modem_version[MODEM_VER_BUF_LEN];
+    char* modem_version;
     int ret;
+    struct tm tm1, tm2;
 
-    ret = get_tz_version(current_tz_version, TZ_VER_BUF_LEN);
+    ret = get_modem_version(current_modem_version, MODEM_VER_BUF_LEN);
     if (ret) {
         return ErrorAbort(state, kVendorFailure,
-                "%s() failed to read current TZ version: %d", name, ret);
+                "%s() failed to read current MODEM build time-stamp: %d", name, ret);
     }
 
-    std::vector<std::string> args;
-    if (!ReadArgs(state, argv, &args)) {
-        return ErrorAbort(state, kArgsParsingFailure,
-                "%s() error parsing arguments", name);
+    memset(&tm1, 0, sizeof(tm));
+    strptime(current_modem_version, "%Y-%m-%d %H:%M:%S", &tm1);
+
+    ret = ReadArgs(state, argv, 1, &modem_version);
+    if (ret < 0) {
+        return ErrorAbort(state, kArgsParsingFailure, "%s() error parsing arguments", name);
     }
 
-    ret = 0;
-    for (auto& tz_version : args) {
-        if (strncmp(tz_version.c_str(), current_tz_version, strlen(tz_version.c_str())) == 0) {
-            ret = 1;
-            break;
-        }
+    uiPrintf(state, "Checking for MODEM build time-stamp %s\n", modem_version);
+
+    memset(&tm2, 0, sizeof(tm));
+    strptime(modem_version, "%Y-%m-%d %H:%M:%S", &tm2);
+
+    if (mktime(&tm1) >= mktime(&tm2)) {
+        ret = 1;
     }
+
+    free(modem_version);
 
     return StringValue(strdup(ret ? "1" : "0"));
 }
 
 void Register_librecovery_updater_X00TD() {
-    RegisterFunction("X00TD.verify_trustzone", VerifyTrustZoneFn);
+    RegisterFunction("X00T.verify_modem", VerifyModemFn);
 }

releasetools.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2009 The Android Open Source Project
 # Copyright (c) 2011, The Linux Foundation. All rights reserved.
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +17,24 @@
 import hashlib
 import common
 import re
+
+def FullOTA_Assertions(info):
+  AddModemAssertion(info)
+  return
+
+def IncrementalOTA_Assertions(info):
+  AddModemAssertion(info)
+  return
+
+def AddModemAssertion(info):
+  android_info = info.input_zip.read("OTA/android-info.txt")
+  m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
+  if m:
+    version = m.group(1).rstrip()
+    if len(version) and '*' not in version:
+      cmd = 'assert(X00T.verify_modem("' + version + '") == "1");'
+      info.script.AppendExtra(cmd)
+  return
 
 def FullOTA_InstallEnd(info):
   info.script.AppendExtra('ifelse(is_mounted("/system"), unmount("/system"));');

Rasenkai/device_asus_X00TD-old

Signed-off-by: Rahul Krishna <Rahulkrishna585@gmail.com>
Signed-off-by: Rasenkai <rasenkai99@gmail.com>

recovery/recovery_updater.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016, The CyanogenMod Project
+ * Copyright (C) 2015, The CyanogenMod Project
  * Copyright (C) 2017, The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,22 +24,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
-#include <string>
-#include <vector>
-
 #include "edify/expr.h"
-#include "otautil/error_code.h"
+#include "updater/install.h"
 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
 #define ALPHABET_LEN 256
 
-#define TZ_PART_PATH "/dev/block/bootdevice/by-name/tz"
-#define TZ_VER_STR "QC_IMAGE_VERSION_STRING="
-#define TZ_VER_STR_LEN 24
-#define TZ_VER_BUF_LEN 255
+#ifdef USES_BOOTDEVICE_PATH
+#define MODEM_PART_PATH "/dev/block/bootdevice/by-name/modem"
+#else
+#define MODEM_PART_PATH "/dev/block/platform/soc/c0c4000.sdhci/by-name/modem"
+#endif
+#define MODEM_VER_STR "Time_Stamp\": \""
+#define MODEM_VER_STR_LEN 14
+#define MODEM_VER_BUF_LEN 20
 
 /* Boyer-Moore string search implementation from Wikipedia */
 
@@ -119,75 +121,81 @@ static char * bm_search(const char *str, size_t str_len, const char *pat,
     return NULL;
 }
 
-static int get_tz_version(char *ver_str, size_t len) {
+static int get_modem_version(char *ver_str, size_t len) {
     int ret = 0;
     int fd;
-    int tz_size;
-    char *tz_data = NULL;
+    int modem_size;
+    char *modem_data = NULL;
     char *offset = NULL;
 
-    fd = open(TZ_PART_PATH, O_RDONLY);
+    fd = open(MODEM_PART_PATH, O_RDONLY);
     if (fd < 0) {
         ret = errno;
         goto err_ret;
     }
 
-    tz_size = lseek64(fd, 0, SEEK_END);
-    if (tz_size == -1) {
+    modem_size = lseek64(fd, 0, SEEK_END);
+    if (modem_size == -1) {
         ret = errno;
         goto err_fd_close;
     }
 
-    tz_data = (char *) mmap(NULL, tz_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (tz_data == (char *)-1) {
+    modem_data = (char *) mmap(NULL, modem_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (modem_data == (char *)-1) {
         ret = errno;
         goto err_fd_close;
     }
 
-    /* Do Boyer-Moore search across TZ data */
-    offset = bm_search(tz_data, tz_size, TZ_VER_STR, TZ_VER_STR_LEN);
+    /* Do Boyer-Moore search across MODEM data */
+    offset = bm_search(modem_data, modem_size, MODEM_VER_STR, MODEM_VER_STR_LEN);
     if (offset != NULL) {
-        strncpy(ver_str, offset + TZ_VER_STR_LEN, len);
+        snprintf(ver_str, len, "%s", offset + MODEM_VER_STR_LEN);
     } else {
         ret = -ENOENT;
     }
 
-    munmap(tz_data, tz_size);
+    munmap(modem_data, modem_size);
 err_fd_close:
     close(fd);
 err_ret:
     return ret;
 }
 
-/* verify_trustzone("TZ_VERSION", "TZ_VERSION", ...) */
-Value * VerifyTrustZoneFn(const char *name, State *state,
-                     const std::vector<std::unique_ptr<Expr>>& argv) {
-    char current_tz_version[TZ_VER_BUF_LEN];
+/* verify_modem("MODEM_VERSION") */
+Value * VerifyModemFn(const char *name, State *state, int argc, Expr *argv[]) {
+    char current_modem_version[MODEM_VER_BUF_LEN];
+    char* modem_version;
     int ret;
+    struct tm tm1, tm2;
 
-    ret = get_tz_version(current_tz_version, TZ_VER_BUF_LEN);
+    ret = get_modem_version(current_modem_version, MODEM_VER_BUF_LEN);
     if (ret) {
         return ErrorAbort(state, kVendorFailure,
-                "%s() failed to read current TZ version: %d", name, ret);
+                "%s() failed to read current MODEM build time-stamp: %d", name, ret);
     }
 
-    std::vector<std::string> args;
-    if (!ReadArgs(state, argv, &args)) {
-        return ErrorAbort(state, kArgsParsingFailure,
-                "%s() error parsing arguments", name);
+    memset(&tm1, 0, sizeof(tm));
+    strptime(current_modem_version, "%Y-%m-%d %H:%M:%S", &tm1);
+
+    ret = ReadArgs(state, argv, 1, &modem_version);
+    if (ret < 0) {
+        return ErrorAbort(state, kArgsParsingFailure, "%s() error parsing arguments", name);
     }
 
-    ret = 0;
-    for (auto& tz_version : args) {
-        if (strncmp(tz_version.c_str(), current_tz_version, strlen(tz_version.c_str())) == 0) {
-            ret = 1;
-            break;
-        }
+    uiPrintf(state, "Checking for MODEM build time-stamp %s\n", modem_version);
+
+    memset(&tm2, 0, sizeof(tm));
+    strptime(modem_version, "%Y-%m-%d %H:%M:%S", &tm2);
+
+    if (mktime(&tm1) >= mktime(&tm2)) {
+        ret = 1;
     }
+
+    free(modem_version);
 
     return StringValue(strdup(ret ? "1" : "0"));
 }
 
 void Register_librecovery_updater_X00TD() {
-    RegisterFunction("X01BD.verify_trustzone", VerifyTrustZoneFn);
+    RegisterFunction("X00TD.verify_modem", VerifyModemFn);
 }

releasetools.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2009 The Android Open Source Project
 # Copyright (c) 2011, The Linux Foundation. All rights reserved.
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,19 +19,19 @@ import common
 import re
 
 def FullOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.input_zip)
+  AddModemAssertion(info)
   return
 
 def IncrementalOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.target_zip)
+  AddModemAssertion(info)
   return
 
-def AddTrustZoneAssertion(info, input_zip):
+def AddModemAssertion(info):
   android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-trustzone\s*=\s*(\S+)', android_info)
+  m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
   if m:
-    versions = m.group(1).split('|')
-    if len(versions) and '*' not in versions:
-      cmd = 'assert(X00TD.verify_trustzone(' + ','.join(['"%s"' % tz for tz in versions]) + ') == "1");'
+    version = m.group(1).rstrip()
+    if len(version) and '*' not in version:
+      cmd = 'assert(X00TD.verify_modem("' + version + '") == "1");'
       info.script.AppendExtra(cmd)
   return